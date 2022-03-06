988ba7b97ce43c9f6ec9a3b1afd6571c1db8728a
Tristan Kermorvant <tristan.kermorvant@gmail.com>
2022-03-06 11:35:41 +0100

    added choose channel command and set akert channel to choosed channel

--- bot.py
+++ bot.py
@@ -9,15 +9,16 @@ with open("config.json", "r") as config:
     data = json.load(config)
     token = data["token"]
 
-bot = commands.Bot()
+bot = commands.Bot(command_prefix="!")
 
 @tasks.loop(seconds=10)
 async def check():
-    channel = bot.get_channel(948168949323923488)
-    name = "Lyubaw"
+    channel = int(data["channels"]["alerts"][:-1][2:])
+    channel = bot.get_channel(channel)
+    name = "Mathox"
     result = await CheckTwitch.check_streamer(CheckTwitch, name)
     if result is not None:
-        message = f"@everyone {name} est en live ! https://twitch.tv/{name}\n{result[0]} : {result[1]}"
+        message = f"everyone {name} est en live ! https://twitch.tv/{name}\n{result[0]} : {result[1]}"
         await channel.send(message)
 
 
@@ -27,6 +28,16 @@ async def on_ready():
     print(f"Pycord v{discord.__version__}")
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.streaming, name="Lyubaw's twitch"))
     check.start()
+
+@bot.command()
+async def choose_channel(ctx, channel = None):
+    if channel is not None:
+        if not "channels" in data:
+            data["channels"] = {}
+        data["channels"]["alerts"] = channel
+
+    with open("config.json", "w") as f:
+        json.dump(data, f, indent=4)
 
 bot.run(token)
 
